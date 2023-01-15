b7161f6d287e22571e7963f950181672273ea3e5
Andrew Zhu <zhua15@mcmaster.ca>
2023-01-15 00:37:12 -0500

    FOR REAL FINAL COMMIT NO MORE

--- server/ai_sketch/ai_sketch.ino
+++ server/ai_sketch/ai_sketch.ino
@@ -1,12 +1,11 @@
 #include <Servo.h>
 Servo right;
 Servo left;
-int pos=0;
 void setup(){
   right.attach(9);
   left.attach(3);
 }
-void loop() {
-  right.write(0);
-  left.write(0);
+void stopBot(){
+    right.write(90);
+    left.write(90);
 }
--- server/base_sketch.ino
+++ server/base_sketch.ino
@@ -1,7 +1,6 @@
 #include <Servo.h>
 Servo right;
 Servo left;
-int pos=0;
 void setup(){
   right.attach(9);
   left.attach(3);
